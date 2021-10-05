d21e62396a849fd82bf2f0823655e8bbc32cede3
diegojfreak <diegorodri.rodriguesdossantos@gmail.com>
2021-10-05 20:38:43 -0300

    Criação de Login

---
+++ ProjetoClinica/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoClinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoClinica.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(MLogin login)
+        {
+            if (ModelState.IsValid)
+                return Ok($"Tentou fazer login com o e-mail {login.Email} e a senha {login.Senha}");
+            else
+                return View();
+
+        }
+    }
+}
