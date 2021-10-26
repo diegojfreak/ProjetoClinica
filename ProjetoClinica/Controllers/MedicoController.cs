2045db88de0e78d980c7d5b8740720a76b408b74
diegojfreak <diegorodri.rodriguesdossantos@gmail.com>
2021-10-25 22:08:47 -0300

    Trabalhando em Medico

---
+++ ProjetoClinica/Controllers/MedicoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoClinica.Data;
+
+namespace ProjetoClinica.Controllers
+{
+    public class MedicoController : Controller
+    {
+        private readonly DataContext context;
+
+        public MedicoController(DataContext dc)
+        {
+            context = dc;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
